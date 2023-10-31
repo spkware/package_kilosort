a6b16783740e664b37b2f0aa7b57e26866cb2c7d
Joao Couto <jpcouto@gmail.com>
2023-10-30 23:59:41 -0700

    Removed graphics and jvm.

--- compile_kilosort2_5.sh
+++ compile_kilosort2_5.sh
@@ -31,7 +31,9 @@ matlab -batch "mexGPUall"
 export OUTDIR=$CWD/$OUTDIR  # output dir
 cd $OUTDIR   # go to the output dir
 
-mcc -m $CWD"/run_"$KILOSORT".m" -a $KILOSORT_PATH -a $NPY_MATLAB_PATH -o $KILOSORT -v
+CMD="mcc -R -nojvm -v -R -nodisplay -m "${CWD}"/run_"${KILOSORT}".m -a "${KILOSORT_PATH}" -a "${NPY_MATLAB_PATH}" -o "${KILOSORT}
+
+matlab -batch "$CMD"
 
 
 
